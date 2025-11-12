14e4bc08013eaa1fe76d8109bc00fe6312499d74
Ledjan Miçani <155323267+Choopin86@users.noreply.github.com>
2025-11-12 13:43:47 +0100

    filter by status UI completed

--- src/todo_manager.js
+++ src/todo_manager.js
@@ -5,17 +5,8 @@ const form = document.querySelector("#todo-form");
 
 init(form, handleTodoCreation);
 //array to store todos
-const todos = [
-  {
-    title: "Buy grocieries",
-    details: "Cheese, milk, eggs",
-    startDate: "2025-11-20",
-    dueDate: "2025-11-21",
-    priority: "mid",
-  },
-];
-//Save todos to localStorage
-localStorage.setItem("savedTodos", JSON.stringify(todos));
+const todos = [];
+
 //Retrive data from localStorage (|| [] -> if there is no todo saved it will start with an empty array)
 const savedTodos = JSON.parse(localStorage.getItem("savedTodos")) || [];
 
@@ -23,7 +14,7 @@ function handleTodoCreation(event) {
   const todo = createTodo(event);
   if (todo) {
     todos.push(todo);
-    //update localStorage with the new todo
+    //update localStorage with the new todos
     localStorage.setItem("savedTodos", JSON.stringify(todos));
   }
 }
